1b7ba3602f1c42ecd8e9eda18c7c39f2c3bd3d41
Zhenyang Xie <xiewak@163.com>
2026-02-25 16:14:48 +0800

    Delete utils directory

--- utils/save_model.py
+++
@@ -1,8 +0,0 @@
-import os
-import torch
-
-
-def save_model(model_path, model, optimizer, current_epoch):
-    out = os.path.join(model_path, "checkpoint_{}.tar".format(current_epoch))
-    state = {'net': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': current_epoch}
-    torch.save(state, out)
